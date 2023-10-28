9592a1d86864a1f9ec3fcfb2eec25193a184e527
Mirko Basic <bejzik8@gmail.com>
2023-10-29 01:22:16 +0200

    Working on deploy script...

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,19 @@
-async function mail() {}
+const { ethers } = require('hardhat')
+
+async function main() {
+    const SimpleStorageFactory =
+        await ethers.getContractFactory('SimpleStorage')
+
+    console.log('Deploying contract...')
+
+    const simpleStorage = await SimpleStorageFactory.deploy()
+
+    await simpleStorage.waitForDeployment()
+
+    const contractAddress = await simpleStorage.getAddress()
+
+    console.log(`Deployed contract to: ${contractAddress}`)
+}
 
 main()
     .then(() => process.exit(0))
