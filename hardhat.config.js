c503b8861369ef62402f5ef0801c2bfb7d2236bc
Mirko Basic <bejzik8@gmail.com>
2023-10-29 01:36:04 +0200

    Added Dotenv package, updated Hardhat configuration.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+    defaultNetwork: 'hardhat',
+    networks: {
+        sepolia: {
+            chainId: 11155111,
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY]
+        }
+    },
     solidity: '0.8.8'
 }
