3c6439434feeb27defe295cc68b064bdaa3b2416
Mirko Basic <bejzik8@gmail.com>
2023-10-29 02:35:23 +0100

    Added Solidity coverage package to the repository.

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,11 +3,12 @@ require('@nomicfoundation/hardhat-verify')
 require('dotenv').config()
 require('./tasks/block-number')
 require('hardhat-gas-reporter')
+require('solidity-coverage')
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ''
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ''
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
