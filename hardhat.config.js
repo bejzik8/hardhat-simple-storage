13918b3ba3ddc2dac76e4ab3ec42b981f93d0532
Mirko Basic <bejzik8@gmail.com>
2023-10-29 02:22:02 +0200

    Added contract verification functionality.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,10 @@
 require('@nomicfoundation/hardhat-toolbox')
+require('@nomicfoundation/hardhat-verify')
 require('dotenv').config()
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -14,5 +16,8 @@ module.exports = {
             accounts: [PRIVATE_KEY]
         }
     },
-    solidity: '0.8.8'
+    solidity: '0.8.8',
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require('hardhat')
+const { ethers, run, network } = require('hardhat')
 
 async function main() {
     const SimpleStorageFactory =
@@ -13,6 +13,34 @@ async function main() {
     const contractAddress = await simpleStorage.getAddress()
 
     console.log(`Deployed contract to: ${contractAddress}`)
+
+    const deploymentTransaction = await simpleStorage.deploymentTransaction()
+
+    console.log('Deployment transaction:', deploymentTransaction)
+
+    console.log('Network config:', network.config)
+
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await deploymentTransaction.wait(6)
+        await verify(contractAddress, [])
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log('Verifying contract...')
+
+    try {
+        await run('verify:verify', {
+            address: contractAddress,
+            constructorArguments: args
+        })
+    } catch (error) {
+        console.log(
+            error.message.toLowerCase().includes('already verified')
+                ? 'Contract already verified'
+                : error
+        )
+    }
 }
 
 main()
